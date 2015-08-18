2b5ea4fbafba9b1e0e8533e2deaaa24ea1091cd0
Martin Virág <martin.virag@eea.sk>
2015-08-18 16:10:26 +0200

    [OpenDataNode/open-data-node#166] fix: not displaying actor name

--- ckanext/odn_theme/plugin.py
+++ ckanext/odn_theme/plugin.py
@@ -1,8 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import ckan.lib.helpers as h
+from ckan.common import c
 from ckan.lib.app_globals import auto_update
 from pylons import config
+from ckan.model.user import User
+import pylons
 
 auto_update.append('ckan.odn_version')
 
@@ -14,6 +17,16 @@ def get_uv_url():
 
 def get_user_management_url():
     return config.get('ckan.user_management.url', '')
+
+def user_display_name():
+    logged_user = c.userobj
+    actor_id = pylons.session['ckanext-cas-actorid']
+    if actor_id:
+        found_user = User.by_name(actor_id)
+        if found_user:
+            return u'{0} ({1})'.format(logged_user.display_name, found_user.display_name)
+    return logged_user.display_name
+
 
 class OdnThemePlugin(plugins.SingletonPlugin):
     '''An comsode theme plugin.
@@ -32,4 +45,6 @@ class OdnThemePlugin(plugins.SingletonPlugin):
     def get_helpers(self):
         return {'localized_url_for_static': localized_url_for_static,
                 'get_uv_url': get_uv_url,
-                'get_user_management_url': get_user_management_url}
+                'get_user_management_url': get_user_management_url,
+                'user_display_name':user_display_name}
+
